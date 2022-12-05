f722b1d9b680238db64aa389c8b8f24eca31bdc3
Md Sayem Hossain <sayemawasome@gmail.com>
2022-12-05 09:40:21 +0600

    web view to pdf export

---
+++ web2pdf/web2pdf/web2pdf/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using GemBox.Document;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using web2pdf.Models;
+using Rotativa.AspNetCore; 
+
+namespace web2pdf.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+
+        public IActionResult Export( )
+        {
+            return new ViewAsPdf("Export")
+            {
+                //  FileName = "Export.pdf", 
+                PageMargins = { Left = 0, Right = 0, Top = 0, Bottom = 0 }, 
+            };
+        }
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
